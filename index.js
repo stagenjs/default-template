56ebc9fb65167956d10198cceb8f7dea04d59f98
Norman Breau <norman@nbsolutions.ca>
2021-02-09 23:43:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var Path = require('path');
+
+module.exports = () => {
+	return Path.resolve(__dirname);
+};
+
---
+++ scripts/utilities.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    formatDate: (date) => {
+        return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+    }
+}
